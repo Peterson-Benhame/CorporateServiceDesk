1cf155631e46ae87d907a8d9e36b7280ba327348
Peterson-Benhame <petersonbrainstormgames@gmail.com>
2026-07-27 18:12:52 -0300

    feat: enable Swagger in production

--- src/CorporateServiceDesk.Api/Program.cs
+++ src/CorporateServiceDesk.Api/Program.cs
@@ -54,7 +54,7 @@ var app = builder.Build();
 
 app.UseExceptionHandler();
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
